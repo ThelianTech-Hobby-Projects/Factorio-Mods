5975e39f18339dbbc2f6d4658615b3df16330d71
ThelianTech <29005715+ThelianTech@users.noreply.github.com>
2024-11-30 16:31:14 -0700

    Initial Mod Creation, Mod Structure for Porting FluidTrainsAPI, SteamTrains Mods to Factorio 2.0

---
+++ TrainMods/locomotive-fuels-api/settings.lua
@@ -0,0 +1,10 @@
+data:extend({
+    --Example Startup Setting
+    {
+        type = "bool-setting",
+        name = "example-setting",
+        setting_type = "startup",
+        default_value = false,
+        hidden = true
+    },
+})
