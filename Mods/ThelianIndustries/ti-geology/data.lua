4242d82ec067bd46461edde1724fa6e6c53ee87e
ThelianTech <29005715+ThelianTech@users.noreply.github.com>
2026-09-06 15:08:39 -0600

    Added ti-geology mod

---
+++ Mods/ThelianIndustries/ti-geology/data.lua
@@ -0,0 +1,3 @@
+    --Item Prototypes
+    --Recipe
+    --Technology
---
+++ Mods/ThelianIndustries/ti-geology/settings.lua
@@ -0,0 +1,10 @@
+data:extend({
+    {
+        type = "bool-setting",
+        name = "ti-debug-mode",
+        setting_type = "startup",
+        default_value = false,
+        hidden = true
+    }
+    -- Startup Settings
+})
